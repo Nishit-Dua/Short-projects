4e1c31352f39ddead6c9021fb66e9e59aba6ceb9
Nishit Dua <nishitdua175@gmail.com>
2020-07-02 19:00:24 +0530

    Added DocStrings and documented a bit ig

--- Image-to-ascii/Image-to-ascii.py
+++ Image-to-ascii/Image-to-ascii.py
@@ -1,11 +1,19 @@
 import PIL.Image as Image
-
+# image_path = '/home/lemark/Downloads/WhatsApp Image 2020-07-02 at 6.00.37 PM.jpeg'
 image_path = 'Full path to your Image file'
-ASCII_CHARS = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "."]
+ASCII_CHARS = ["@", "#", "S", "X", "*", "+", "^", ";", ":", ",", "."]
+new_width = 100
 
 
 def resize(image):
-    new_width = 100
+    '''
+    Resizes the PIL.Image object to a new height and width
+    to change your output change new_width variable defined
+    above
+
+    Return a PIL.Image object
+    '''
+
     width, height = image.size
     aspect_ratio = height/width
     new_height = int(aspect_ratio*new_width)
@@ -13,18 +21,34 @@ def resize(image):
 
 
 def Grey(image):
+    '''
+    Returns a Monocromatic greyscale PIL.Image
+    '''
     return image.convert(mode="L")
 
 
 def image_to_ascii(image):
+    '''
+    takes a monochannel PIL.Image object, gets the pixel val
+    (which is always < 255) and gets the questiont val of 25
+    (b/w 0 and 10), if you want to change the Ascii characters
+    in your image file change ASCII_CHARS list 'high intensity'
+    characters first and 11 string constituents.
+    '''
     data = image.getdata()
     return ''.join(ASCII_CHARS[val//25] for val in data)
 
 
 def main():
+    '''
+    Writes the output generated in "Ascii-Image.txt" in the
+    current directory file if you wish to save your previous
+    txt file rename it or move it out of that directory as it
+    will overwrites it.
+    '''
     try:
         image = Image.open(image_path)
-    except:
+    except Exception:
         print('Not a valid image path')
         return
 
@@ -32,10 +56,12 @@ def main():
     text = image_to_ascii(new_img)
     pix_count = len(text)
 
-    final_ascii = '\n'.join(text[i:i+100] for i in range(0, pix_count, 100))
+    final_ascii = '\n'.join(text[i:i+new_width]
+                            for i in range(0, pix_count, new_width))
 
     with open('Ascii-Image.txt', 'w') as f:
         f.write(final_ascii)
 
 
-main()
+if __name__ == "__main__":
+    main()
